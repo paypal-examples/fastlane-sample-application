8920f437440d092a54c91dfd8be49fb9b3fa3de7
Louis Low <jinlow@paypal.com>
2024-08-31 18:20:20 +0800

    add devcontainers for client and server
    Stop tracking .vscode/settings.json
    Stop tracking .devcontainer/update_settings.sh
    show shared folders only when HTML is selected
    add steps to run codespace in README.md

--- .devcontainer/update_settings.sh
+++ .devcontainer/update_settings.sh
@@ -3,16 +3,25 @@
 set -e
 
 VISIBLE_FOLDER_SERVER="$VISIBLE_FOLDER_SERVER"
+VISIBLE_FOLDER_CLIENT="$VISIBLE_FOLDER_CLIENT"
+VISIBLE_FOLDER_DEVCONTAINER="${VISIBLE_FOLDER_CLIENT}_${VISIBLE_FOLDER_SERVER}"
+
+if [ -z "$VISIBLE_FOLDER_CLIENT" ]; then
+    echo "Error: VISIBLE_FOLDER_CLIENT is not set, setting it to default"
+    VISIBLE_FOLDER_CLIENT="DEFAULT"
+fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 WORKSPACE_DIR="$( cd "$SCRIPT_DIR/.." &> /dev/null && pwd )"
 SERVER_DIR="$WORKSPACE_DIR/server"
+CLIENT_DIR="$WORKSPACE_DIR/client"
 DEVCONTATINER_DIR="$WORKSPACE_DIR/.devcontainer"
 SETTINGS_FILE="$WORKSPACE_DIR/.vscode/settings.json"
 
 echo "Workspace directory: $WORKSPACE_DIR"
 echo "Server directory: $SERVER_DIR"
 echo "Visible server folder: $VISIBLE_FOLDER_SERVER"
+echo "Visible client folder: $VISIBLE_FOLDER_CLIENT"
 
 if [ ! -d "$SERVER_DIR" ]; then
     echo "Error: Server directory not found at $SERVER_DIR"
@@ -37,7 +46,7 @@ echo "{
 first=true
 for dir in "$SERVER_DIR"/*/ ; do
     dir_name=$(basename "$dir")
-    if [ -d "$dir" ] && [ "$dir_name" != "$VISIBLE_FOLDER_SERVER" ] && [ "$dir_name" != "shared" ]; then
+    if [ -d "$dir" ] && [ "$dir_name" != "$VISIBLE_FOLDER_SERVER" ] && ([ "$dir_name" != "shared" ] || [ "$VISIBLE_FOLDER_CLIENT" != "html" ]); then
         if [ "$first" = true ] ; then
             first=false
         else
@@ -49,7 +58,7 @@ done
 
 for dir in "$DEVCONTATINER_DIR"/*/ ; do
     dir_name=$(basename "$dir")
-    if [ -d "$dir" ] && [ "$dir_name" != "$VISIBLE_FOLDER_SERVER" ]; then
+    if [ -d "$dir" ] && [ "$dir_name" != "$VISIBLE_FOLDER_SERVER" ] && [ "$dir_name" != "$VISIBLE_FOLDER_DEVCONTAINER" ]; then
         if [ "$first" = true ] ; then
             first=false
         else
@@ -59,10 +68,22 @@ for dir in "$DEVCONTATINER_DIR"/*/ ; do
     fi
 done
 
+for dir in "$CLIENT_DIR"/*/ ; do
+    dir_name=$(basename "$dir")
+    if [ -d "$dir" ] && [ "$dir_name" != "$VISIBLE_FOLDER_CLIENT" ]; then
+        if [ "$first" = true ] ; then
+            first=false
+        else
+            echo "," >> "$SETTINGS_FILE"
+        fi
+        echo -n "    \"**/client/$dir_name\": true" >> "$SETTINGS_FILE"
+    fi
+done
+
 echo "
   }
 }" >> "$SETTINGS_FILE"
 
-echo "VS Code settings updated to show only $VISIBLE_FOLDER_SERVER folder in server directory."
+echo "VS Code settings updated to show only $VISIBLE_FOLDER_SERVER and $VISIBLE_FOLDER_CLIENT folder in server directory."
 echo "Contents of $SETTINGS_FILE:"
 cat "$SETTINGS_FILE"
